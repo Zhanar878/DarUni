49513aa89cc6b598961999a6188975c58f8b4959
Zhanar <zh.dosymbekova@gmail.com>
2022-12-08 23:31:48 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,125 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        int[] number = {1,3,4,1,5,5};
+        float sum=0;
+        for (int x : number) {
+            sum += x;
+            System.out.println("Среднее значение = "+sum / number.length);
+        }}
+
+        int[] myArray = {1,3,4,1,5,5};
+       for (int i=0; i<6; i++){
+            for (int j = i+1; j < 6; j++) {
+                if (myArray[i]==myArray[j]) {
+                    System.out.println(j);
+                }
+            }
+        }}}
+
+        for (int i=1; i<=5; i++){
+            System.out.print(i+ "");
+        }
+
+
+
+        for (int i=1; i<=10; i++){
+            System.out.println("3 *" + i +"="+3*i);
+        }
+
+
+        System.out.print("Введите любое целое положительное число: ");
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int sum=0;
+        for (int i=1; i<=n; i++) {
+            sum = sum + i;
+        }
+            System.out.println(sum);
+
+
+        int a=7;
+        while (a<=105) {
+            System.out.print(a + " ");
+            a+=7;
+            }
+
+
+        int n=1;
+        while (n<=512){
+            System.out.print( n+" ");
+            n*=2;
+            }
+
+
+        for (int i=0; i<3; i++){
+            for (int j=0; j<5; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+
+        int n0=1;
+        int n1=1;
+        int n2;
+        System.out.print(n0+" "+n1+" ");
+        for (int i=3; i<=11; i++){
+            n2=n0+n1;
+            System.out.print(n2+" ");
+            n0=n1;
+            n1=n2;
+        }
+        System.out.println();
+}
+
+        String answer;
+        boolean exit;
+        System.out.println("Что это такое: синий, большой, с усами и полностью набит яйцами?");
+        Scanner inputAnswer=new Scanner(System.in);
+        exit=false;
+        for (int i=1; i<=3; i++){
+            if (exit==true)
+                break;
+            answer=inputAnswer.next();
+            switch (answer){
+                case ("Троллейбус"):
+                    System.out.println("Правильно!");
+                exit=true;
+                break;
+                case ("Сдаюсь"):
+                    System.out.println("Правильный ответ: Троллейбус.");
+                    exit=true;
+                    break;
+                default:
+                    System.out.println("Подумай ещё.");
+
+            }
+
+int a=0;
+while (a<10) {
+
+
+    if (a==4) {
+        System.out.println("Вот счастливый пельмень!");
+        break;
+    }
+        a++;
+    }
+*/
+        
+}}
+
+
+
+
+
+
+
+
+
+    
+
